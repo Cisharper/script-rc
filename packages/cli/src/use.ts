9ace7c4ca9931987a5a4bd0c8e0f903201ed4f17
Александр <kbrais007@gmail.com>
2022-05-09 00:09:21 +0200

    use up

--- packages/cli/src/use.ts
+++ packages/cli/src/use.ts
@@ -1,16 +1,24 @@
 import { mintNFT } from './script-rc';
 import { PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import {
+    getProgramAccounts,
+    loadCandyProgramV2,
+    loadWalletKey,
+    AccountAndPubkey,
+    deriveCandyMachineV2ProgramAddress,
+    getCollectionPDA,
+} from './helpers/accounts';
 async function main() {
 
 //inputs
-  let eth_str_json = '{	"name": "Royal Crow #527",	"description": "The Royal Crows Club has just thrown its grand opening party, with way too much champagne and confetti. It will be revealed soon!",	"image": "ipfs://QmcCBYuWNoobxUchnRAhanRpaK1XbvJP5YjqfMDQpCLrMJ/1.jpg",	"external_url": "",	"tokenId": 2,	"edition": 2,	"attributes": [{		"trait_type": "unrevealed",		"value": "unrevealed"}]}';
-  const keypair = '/wallet/dev-rc.json';
+    const keypair = loadWalletKey('/wallet/dev-rc.json');
+
   const env = 'devnet';
   let orig_sig = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
   let UserWallet = "3btVre38gtUQ8GXGNfePZEpeBqUy8z5cuwD5xpP2avoh";
   let collection_verify = "FeEFHu5Pbkied7zTtDYzQVyw6RAFFZVNKXjX3keC4XAG";
   let treasuryWallet = new PublicKey("7osbA4vs7gKtSf7JyNUgVJtTQz4SRg5gHgVvWnkMKnqn");
-    let url_json = "";
+  let url_json = "https://xxxxxxx";
 
  //await mintNFT(url_json,keypair,env,orig_sig,UserWallet,collection_verify,treasuryWallet);
 
