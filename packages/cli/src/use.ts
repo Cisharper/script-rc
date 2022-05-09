7eea6efe7356eb283f2de47ca6d5fc9290ce0376
Александр <kbrais007@gmail.com>
2022-05-09 18:46:55 +0200

    use up

--- packages/cli/src/use.ts
+++ packages/cli/src/use.ts
@@ -17,10 +17,11 @@ async function main() {
   let orig_sig = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
   let UserWallet = "3btVre38gtUQ8GXGNfePZEpeBqUy8z5cuwD5xpP2avoh";
   let collection_verify = "FeEFHu5Pbkied7zTtDYzQVyw6RAFFZVNKXjX3keC4XAG";
-  let treasuryWallet = new PublicKey("7osbA4vs7gKtSf7JyNUgVJtTQz4SRg5gHgVvWnkMKnqn");
+    let treasuryWallet = new PublicKey("7osbA4vs7gKtSf7JyNUgVJtTQz4SRg5gHgVvWnkMKnqn");
+    let cache_pth = "/metaplex/1";
   let url_json = "https://xxxxxxx";
-
- //await mintNFT(url_json,keypair,env,orig_sig,UserWallet,collection_verify,treasuryWallet);
+    let url_rpc = "https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899/";
+ //await mintNFT(url_json,keypair,env,orig_sig,UserWallet,collection_verify,treasuryWallet,cache_pth,url_rpc);
 
 
 
